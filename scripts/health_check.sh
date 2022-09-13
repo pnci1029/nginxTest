c2bb0cbb5af046e65f91b106b01957abb125c51f
ChHong <pnci1029@gmail.com>
2022-09-13 11:30:22 +0900

    gg

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,15 +1,14 @@
 # health_check.sh
 
+#!/bin/bash
 
 # Crawl current connected port of WAS
-# shellcheck disable=SC2002
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 # Toggle port Number
-if [ "${CURRENT_PORT}" -eq 8081 ]; then
+if [ ${CURRENT_PORT} -eq 8081 ]; then
     TARGET_PORT=8082
-
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
     TARGET_PORT=8081
 else
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,8 +1,8 @@
 # switch.sh
 
+#!/bin/bash
 
 # Crawl current connected port of WAS
-# shellcheck disable=SC2002
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
